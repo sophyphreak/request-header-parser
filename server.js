c49f6945fb3054512239cbadd5aeaba920945a1d
Andrew <arhorn@smcm.edu>
2018-12-18 23:41:39 +0700

    fixed bugs in whoami request

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const port = process.env.PORT || 3000;
 
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));
@@ -10,28 +11,20 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
 app.get("/api/whoami", (req, res) => {
-  const xForwardedFor = req.headers['x-forwarded-for'];
-  const forwardedForArray = xForwardedFor.split(',');
-  const ip = forwardedForArray[0];
-  
-  const acceptLanguage = req.headers['accept-language'];
-  const userAgent = req.headers['user-agent']
+  const language = req.get('accept-language');
+  const ipaddress = req.ip.slice(7);
+  const software = req.get('user-agent');
   
   const returnedJson = {
-    ipaddress: ip,
-    language: acceptLanguage,
-    software: userAgent
+    ipaddress,
+    language,
+    software
   }
   
   res.json(returnedJson);
 });
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(port, () => {
+  console.log(`Server is up on ${port}!`);
 });
